d0bf6c37358875d304a42795f8fb9dba75515614
PaengE <ckddn2820@gmail.com>
2020-12-28 01:22:04 +0900

    ch3_Spring Data JPA 적용 및 테스트

---
+++ src/test/java/com/yneerg/admin/book/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.yneerg.admin.book.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void save() {
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder().title(title).content(content).author("qwerty@gmail.com").build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        Assertions.assertEquals(posts.getTitle(), title);
+        Assertions.assertEquals(posts.getContent(), content);
+    }
+}
