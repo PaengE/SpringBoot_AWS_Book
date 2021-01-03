2c6b51243618307150b84349fae539b1edcd1de1
PaengE <ckddn2820@gmail.com>
2021-01-03 21:57:45 +0900

    게시글 수정 기능 추가

--- src/main/java/com/yneerg/admin/book/web/IndexController.java
+++ src/main/java/com/yneerg/admin/book/web/IndexController.java
@@ -1,18 +1,38 @@
 package com.yneerg.admin.book.web;
 
+import com.yneerg.admin.book.service.PostsService;
+import com.yneerg.admin.book.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class IndexController {
+    private final PostsService postsService;
+
+    public IndexController(PostsService postsService) {
+        this.postsService = postsService;
+    }
+
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
--- src/main/java/com/yneerg/admin/book/web/dto/PostsResponseDto.java
+++ src/main/java/com/yneerg/admin/book/web/dto/PostsResponseDto.java
@@ -3,8 +3,8 @@ package com.yneerg.admin.book.web.dto;
 import com.yneerg.admin.book.domain.posts.Posts;
 import lombok.Getter;
 
-import java.awt.*;
-import java.net.ContentHandler;
+import java.time.LocalDateTime;
+
 
 @Getter
 public class PostsResponseDto {
@@ -12,11 +12,13 @@ public class PostsResponseDto {
     private String title;
     private String content;
     private String author;
+    private LocalDateTime modifiedDate;
 
     public PostsResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.content = entity.getContent();
         this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
     }
 }
