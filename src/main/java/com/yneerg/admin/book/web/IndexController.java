f6948b96ddfa709a7b54988c6b113bc2ea4b13b7
PaengE <ckddn2820@gmail.com>
2021-01-06 01:31:11 +0900

    구글 로그인 연동

--- src/main/java/com/yneerg/admin/book/web/IndexController.java
+++ src/main/java/com/yneerg/admin/book/web/IndexController.java
@@ -1,24 +1,32 @@
 package com.yneerg.admin.book.web;
 
+import com.yneerg.admin.book.config.auth.dto.SessionUser;
 import com.yneerg.admin.book.service.PostsService;
 import com.yneerg.admin.book.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
-
-    public IndexController(PostsService postsService) {
-        this.postsService = postsService;
-    }
-
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
@@ -33,6 +41,4 @@ public class IndexController {
         model.addAttribute("post", dto);
         return "posts-update";
     }
-
-
 }
